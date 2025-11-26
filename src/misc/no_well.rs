6ccfcf1172aa2728d5ba0624d0bb59c50380e830
Alphys <j.zhiyun@icloud.com>
2025-11-26 12:07:59 -0500

    Disable Evil Chapters

--- src/misc/no_well.rs
+++ src/misc/no_well.rs
@@ -6,6 +6,14 @@ fn item_exchange_menu_build_attribute(_this: i64, _method_info: OptionalMethod) 
     menu::DISABLED
 }
 
+#[skyline::hook(offset = 0x01b2d6d0)]
+fn evil_map_start_menu_build_attribute(_this: i64, _method_info: OptionalMethod) -> i32 {
+    menu::DISABLED
+}
+
 pub fn install() {
-    skyline::install_hook!(item_exchange_menu_build_attribute);
+    skyline::install_hooks!(
+        item_exchange_menu_build_attribute,
+        evil_map_start_menu_build_attribute
+    );
 }
